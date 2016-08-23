6e9530bc501855c8195c0af69ecbdf5fed065c32
Michael Kulik <mishkulik@gmail.com>
2016-08-23 15:30:23 +0700

    Start 11 lesson

---
+++ lesson11/Ads.php
@@ -0,0 +1,198 @@
+<?php
+
+class Ads
+{
+    // name of table
+    protected $table = 'ads';
+
+    // columns in the table
+    private $id;
+    private $title;
+    private $description;
+    private $seller_name;
+    private $email;
+    private $phone;
+    private $price;
+    private $role;
+    private $allow_mails;
+    private $city_id;
+    private $category_id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSellerName()
+    {
+        return $this->seller_name;
+    }
+
+    /**
+     * @param mixed $seller_name
+     */
+    public function setSellerName($seller_name)
+    {
+        $this->seller_name = $seller_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllowMails()
+    {
+        return $this->allow_mails;
+    }
+
+    /**
+     * @param mixed $allow_mails
+     */
+    public function setAllowMails($allow_mails)
+    {
+        $this->allow_mails = $allow_mails;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCityId()
+    {
+        return $this->city_id;
+    }
+
+    /**
+     * @param mixed $city_id
+     */
+    public function setCityId($city_id)
+    {
+        $this->city_id = $city_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryId()
+    {
+        return $this->category_id;
+    }
+
+    /**
+     * @param mixed $category_id
+     */
+    public function setCategoryId($category_id)
+    {
+        $this->category_id = $category_id;
+    }
+
+
+}
---
+++ lesson11/Db.php
@@ -0,0 +1,10 @@
+<?php
+
+// В этом классе будут несколько методов для работы с базой данных
+class Db
+{
+    public function fetchAds(Ads $id, PDO $connection)
+    {
+        $sql = 'SELECT * FROM';
+    }
+}

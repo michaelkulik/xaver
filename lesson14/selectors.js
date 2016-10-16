7266f68b3c876b177a0f0ef9b19bc82248f67ea2
Michael <mishkulik@gmail.com>
2016-10-16 18:35:58 +0700

    Fix a mistake

--- lesson14/selectors.js
+++ lesson14/selectors.js
@@ -1,6 +1,6 @@
 $(function(){
     $('ul.myList a:contains(CSS)'); // 1. Все ссылки с текстом Css которые находятся в списке
-    $('img:eq(3), img:nth-child(4)'); // 2. 3 и 4 картинки
+    $('img:eq(2), img:nth-child(4)'); // 2. 3 и 4 картинки
     $('tr:first, tr:first + tr', '#languages tbody'); // 3. 1 и 2 строки в таблице (заголовок не учитывать)
     $('#languages tbody td:eq(10)'); /* или */ $('td:odd', '#languages tbody tr:last'); // 4. В последней строке таблицы подсветить ячейку второй колонки
 });
